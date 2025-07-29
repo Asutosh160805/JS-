857c88323755c29219b149d247a93acdfd151ec4
Asutosh Dash <144788991+Asutosh160805@users.noreply.github.com>
2025-07-29 13:06:29 +0000

    datatypes commited :)

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+console.log(3+3);
+console.log("Hello, World!");
+
+let name = "John";//string
+let age = 30;//int
+let isLoggedIn = true;//boolean
+//null is also a data type 
+//undefined =>value not defined
+
+//symbol => unique and immutable data type
+
+console.log(typeof "hello"); // string
+console.log(typeof null);//object
+console.log(typeof undefined); 
