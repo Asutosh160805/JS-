3dc7ec22385eb1bfa04fa62c9ac8295d0035e050
Asutosh Dash <144788991+Asutosh160805@users.noreply.github.com>
2025-07-29 13:42:58 +0000

    updated 03

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -14,3 +14,20 @@ console.log(score4);//NaN => Not a Number
 let isLoggedIn = 1;
 let booleanLoggedIn = Boolean(isLoggedIn);//conversion
 console.log(booleanLoggedIn);//true
+
+//Operations
+
+let value = 3
+let negVALUE = -value;//negation
+console.log(negVALUE);//-3
+
+let str1 = "Hello";
+let str2 = "World";
+let str3 = str1 + " " + str2;//concatenation
+console.log(str3);//Hello World 
+
+console.log("1" + 2);//"12" => string concatenation
+console.log(1 + "2");//"12" => string concatenation
+console.log(1+2+"3");//"33" => string concatenation
+console.log("1" + 2 + 3);//"123" => string concatenation
+
